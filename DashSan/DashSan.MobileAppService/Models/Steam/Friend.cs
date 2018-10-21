9c04898e18d95819fefba8d792f9a27af8569a35
Santeri <snattu91@gmail.com>
2018-10-21 21:35:41 +0300

    Models and controller implementation (#2)

---
+++ DashSan/DashSan.MobileAppService/Models/Steam/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashSan.MobileAppService.Models.Steam
+{
+    public class Friend
+    {
+        public string steamid { get; set; }
+        public string relationship { get; set; }
+        public int friend_since { get; set; }
+    }
+
+    public class Friends
+    {
+        public List<Friend> friends { get; set; }
+    }
+
+}

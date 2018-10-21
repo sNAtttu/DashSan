f6fba9067729e4c0edb5aabcb92d48c679f6bb9e
sNAttu <snattu91@gmail.com>
2018-10-21 16:17:59 +0300

    Add project files.

---
+++ DashSan/DashSan/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DashSan.Models;
+
+namespace DashSan.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

050178953d412dc42b5c86f2ebd9cd9e33610931
sNAttu <snattu91@gmail.com>
2018-10-21 21:56:27 +0300

    Implemented models and controller for achievements

---
+++ DashSan/DashSan.MobileAppService/Models/Steam/Achievement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashSan.MobileAppService.Models.Steam
+{
+    public class Achievement
+    {
+        public string apiname { get; set; }
+        public int achieved { get; set; }
+        public int unlocktime { get; set; }
+    }
+}

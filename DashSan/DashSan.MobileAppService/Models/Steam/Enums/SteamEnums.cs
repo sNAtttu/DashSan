2b494a35bf4c0bd1fff71f22a641f4faf2ce8143
Santeri <snattu91@gmail.com>
2018-10-21 22:21:52 +0300

    Created method enum and refactored url creation from controllers. (#4)

---
+++ DashSan/DashSan.MobileAppService/Models/Steam/Enums/SteamEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashSan.MobileAppService.Models.Steam.Enums
+{
+    public class SteamEnums
+    {
+        public enum Methods
+        {
+            GetPlayerSummaries, GetFriendList, GetPlayerAchievements
+        }
+    }
+}

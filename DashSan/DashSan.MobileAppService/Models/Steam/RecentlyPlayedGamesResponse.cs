8fbb487c75adf92c5b940f8c06b5209eaef0e240
Santeri <snattu91@gmail.com>
2018-10-22 20:46:40 +0300

    Added logger to controller (#9)

--- DashSan/DashSan.MobileAppService/Models/Steam/RecentlyPlayedGamesResponse.cs
+++ DashSan/DashSan.MobileAppService/Models/Steam/RecentlyPlayedGamesResponse.cs
@@ -7,7 +7,13 @@ namespace DashSan.MobileAppService.Models.Steam
 {
     public class RecentlyPlayedGamesResponse
     {
+        public Response response { get; set; }
+    }
+
+    public class Response
+    {
         public int total_count { get; set; }
         public List<Game> games { get; set; }
     }
+
 }

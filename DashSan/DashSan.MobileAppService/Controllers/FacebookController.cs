b363c670d665345b461655bdbf8fd99304e594b2
Santeri <snattu91@gmail.com>
2018-10-23 21:31:22 +0300

    Model and controller created for the FB API (#10)

---
+++ DashSan/DashSan.MobileAppService/Controllers/FacebookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Facebook;
+using DashSan.MobileAppService.Models;
+using DashSan.MobileAppService.Models.Facebook;
+using Microsoft.Extensions.Options;
+
+namespace DashSan.MobileAppService.Controllers
+{
+    [Route("api/facebook")]
+    public class FacebookController : Controller
+    {
+        private readonly ApiKeys _apiKeys;
+
+        private FacebookClient fbClient;
+
+        public FacebookController(IOptions<ApiKeys> apiKeys)
+        {
+            _apiKeys = apiKeys.Value;
+            CreateFacebookClient();
+        }
+
+        private void CreateFacebookClient()
+        {
+            fbClient = new FacebookClient();
+            dynamic result = fbClient.Get("oauth/access_token", new
+            {
+                client_id = _apiKeys.FacebookAppKey,
+                client_secret = _apiKeys.FacebookAppSecret,
+                grant_type = "client_credentials"
+            });
+            fbClient.AccessToken = result.access_token;
+        }
+
+        [HttpGet("GetProfileInformation/{userid}")]
+        [ResponseCache(Duration = 60)]
+        public IActionResult GetProfileInformation(string userid)
+        {
+            FacebookPersonalModel result = fbClient.Get<FacebookPersonalModel>($"/v3.2/{userid}?fields=name");
+            return Ok(result);
+        }
+    }
+}
---
+++ DashSan/DashSan.MobileAppService/Models/Facebook/FacebookPersonalModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashSan.MobileAppService.Models.Facebook
+{
+    public class FacebookPersonalModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
